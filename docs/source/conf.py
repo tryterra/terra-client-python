21fd08551fc9de960b72ff48f80f1634e21ac201
terra-walid <108660129+terra-walid@users.noreply.github.com>
2022-07-27 09:11:47 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))  # Source code dir relative to this file
+sys.path.insert(0, os.path.abspath('../..'))  # Source code dir relative to this file
 
 
 project = 'Python Wrapper for Terra API'
