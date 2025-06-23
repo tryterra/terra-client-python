44044a5bb608628b44d5dab07f86e48105159cbc
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-06-23 14:48:58 +0000

    Release 1.0.3

--- src/terra/core/client_wrapper.py
+++ src/terra/core/client_wrapper.py
@@ -15,10 +15,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "terra-python/1.0.1",
+            "User-Agent": "terra-python/1.0.3",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "terra-python",
-            "X-Fern-SDK-Version": "1.0.1",
+            "X-Fern-SDK-Version": "1.0.3",
         }
         headers["dev-id"] = self._dev_id
         headers["x-api-key"] = self.api_key
